a12f3bfce5417d58f76772cf29ca31316ae584fc
amirho3in <amirhoseinhaerian@gmail.com>
2022-12-26 23:08:11 +0100

    change folder structures

---
+++ Back-End/API.py
@@ -0,0 +1,54 @@
+#Install Libraries
+import os
+import sys
+from flask import Flask, request, jsonify
+import joblib
+import traceback
+import pandas as pd
+import numpy as np
+
+if __name__ == "__main__":
+ try:
+    port = int(sys.argv[1])
+ except:
+    port = 3000
+
+dirname = os.path.abspath('')
+
+# Loading our models
+encoderModel = joblib.load(os.path.join(dirname, "models/encoderModel.pkl")) 
+print ("enodelModel loaded")
+scalerModel = joblib.load(os.path.join(dirname, "models/scalerModel.pkl")) 
+print ("scalerModel loaded")
+X_columns = joblib.load(os.path.join(dirname, "models/X_columns.pkl")) 
+print ("X_columns loaded")
+KNN = joblib.load(os.path.join(dirname, "models/KNN.pkl")) 
+print ("KNN loaded")
+
+application = Flask(__name__)
+
+@application.route("/predict", methods=["POST"])
+def predict():
+    try:
+        # Converting our data into DataFrame
+        req_data = request.json
+        req_df = pd.DataFrame.from_dict(req_data, orient="columns")
+
+        num_col = req_df.describe().columns
+
+        # encoding the categorical data into numerical data
+        for c in list(encoderModel.keys()):    
+            req_df[c] = encoderModel[c].transform(req_df[c])
+
+        # standardize our data
+        mock_data_df_numeric = req_df[num_col]
+        mock_data_df_numeric_st = scalerModel.transform(mock_data_df_numeric)
+        req_df[num_col] = mock_data_df_numeric_st
+
+        # predict the value
+        predicted_value = KNN.predict(req_df)
+        return jsonify({"prediction": str(predicted_value[0])})
+    except:
+        return jsonify({"trace": traceback.format_exc()})
+
+application.run(port=port)
