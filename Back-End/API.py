262fd53cf589123116cb47678604beb34e8a35c4
amirho3in <amirhoseinhaerian@gmail.com>
2023-01-04 04:35:50 +0100

    change encoding model

--- Back-End/API.py
+++ Back-End/API.py
@@ -20,12 +20,14 @@ dirname = os.path.abspath('')
 # Loading our models
 encoderModel = joblib.load(os.path.join(dirname, "Back-End/models/encoderModel.pkl")) 
 print ("enodelModel loaded")
+ohe = joblib.load(os.path.join(dirname, "Back-End/models/ohe.pkl")) 
+print ("ohe loaded")
 scalerModel = joblib.load(os.path.join(dirname, "Back-End/models/scalerModel.pkl")) 
 print ("scalerModel loaded")
-X_columns = joblib.load(os.path.join(dirname, "Back-End/models/X_columns.pkl")) 
+cat_columns = joblib.load(os.path.join(dirname, "Back-End/models/cat_columns.pkl")) 
 print ("X_columns loaded")
-KNN = joblib.load(os.path.join(dirname, "Back-End/models/KNN.pkl")) 
-print ("KNN loaded")
+RF = joblib.load(os.path.join(dirname, "Back-End/models/RF.pkl")) 
+print ("RF loaded")
 
 application = Flask(__name__)
 application.config['SECRET_KEY'] = 'secret key'
@@ -43,10 +45,14 @@ def predict():
         num_col = req_df.describe().columns
 
         # encoding the categorical data into numerical data
-        for c in list(encoderModel.keys()):    
-            req_df[c] = encoderModel[c].transform(req_df[c])
+        # for c in list(encoderModel.keys()):    
+        #     req_df[c] = encoderModel[c].transform(req_df[c])
 
-        print(req_df)
+        cat_ohe = ohe.transform(req_df[cat_columns])
+        ohe_df = pd.DataFrame(cat_ohe, columns = ohe.get_feature_names(input_features = cat_columns))
+        req_df = pd.concat([ohe_df, req_df], axis=1).drop(columns = cat_columns, axis=1)
+
+        # req_df = pd.get_dummies(req_df, drop_first=True)
 
         # standardize our data
         mock_data_df_numeric = req_df[num_col]
@@ -54,7 +60,7 @@ def predict():
         req_df[num_col] = mock_data_df_numeric_st
 
         # predict the value
-        predicted_value = KNN.predict(req_df)
+        predicted_value = RF.predict(req_df)
         return jsonify({"prediction": str(predicted_value[0])})
     except:
         return jsonify({"trace": traceback.format_exc()})
